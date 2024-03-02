be408235b52ec28f870749d7e02fdaab8cbe2321
Tristan Laulanie <laulanietristan@gmail.com>
2024-03-02 12:04:41 +0100

    categories:responsive:done

--- src/components/Dashboard/Categories/index.js
+++ src/components/Dashboard/Categories/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./Categories.css";
+import "./CategoriesResponsive.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPenSquare } from "@fortawesome/free-solid-svg-icons";
 import { faTrash } from "@fortawesome/free-solid-svg-icons";
@@ -11,13 +12,14 @@ function Categories() {
       <body className="categories_body">
         <section id="title">
           <h2>Table des Catégories</h2>
-          <i className="fa-solid fa-pen-to-square"></i>
         </section>
 
         <section className="categories_section">
           <div className="categories_tables">
             <h3 className="categories_title_info">Id</h3>
-            <h3 className="categories_title_info">Catégorie</h3>
+            <h3 className="categories_title_info ">
+              <div className="category_title">Catégorie</div>
+            </h3>
             <h3 className="categories_title_info">image</h3>
             <p className="categories_set_icons"></p>
           </div>
@@ -45,8 +47,8 @@ function Categories() {
           </div>
           <div className="categories_tables">
             <h3 className="categories_title_info">22</h3>
-            <h3 className="categories_title_info">Enfants</h3>
-            <h3 className="categories_title_info">Children</h3>
+            <h3 className="categories_title_info">Enfant</h3>
+            <h3 className="categories_title_info">Child</h3>
             <p className="categories_set_icons">
               <Link to="/dashboard/categories/set">
                 <FontAwesomeIcon icon={faPenSquare} className="pen-square" />
